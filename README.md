1967e7fa59c84f905fd01337b65282ce9301837c
DefaultXYZ <yevhenii.ishchenko@gmail.com>
2017-04-20 23:29:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# This is sample application built on React Native
---
+++ index.ios.js
@@ -0,0 +1,57 @@
+'use strict';
+
+import React, {Component} from "react";
+import {AppRegistry, StyleSheet, Text, View} from "react-native";
+
+const text = {
+    color: '#FFF',
+    fontWeight: 'bold',
+    textAlign: 'center'
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#000'
+    },
+    content: {
+        flex: 9,
+        justifyContent: 'center'
+    },
+    footer: {
+        flex: 1
+    },
+    headerText: {
+        ...text,
+        fontSize: 42
+    },
+    contentText: {
+        ...text,
+        fontSize: 18
+    },
+    footerText: {
+        ...text,
+        flex: 1,
+        marginBottom: 5,
+        fontSize: 12,
+        textAlignVertical: 'bottom'
+    }
+});
+
+export default class MainApp extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.content}>
+                    <Text style={styles.headerText}>Hello#_#World</Text>
+                    <Text style={styles.contentText}>It is sample application.</Text>
+                </View>
+                <View style={styles.footer}>
+                    <Text style={styles.footerText}>Powered by React Native Framework.</Text>
+                </View>
+            </View>
+        );
+    }
+}
+
+AppRegistry.registerComponent('SampleApplication', () => MainApp);
