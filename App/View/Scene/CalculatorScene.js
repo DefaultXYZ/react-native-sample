473c761e8d4df7616647f08f8b6f85c2cee43f10
DefaultXYZ <yevhenii.ishchenko@gmail.com>
2017-05-07 11:51:27 +0200

    Calculator works with redux

--- App/View/Scene/CalculatorScene.js
+++ App/View/Scene/CalculatorScene.js
@@ -1,10 +1,74 @@
 'use strict';
 
 import React, {Component} from "react";
-import {Button, TextInput, ToastAndroid, View} from "react-native";
+import {Button, TextInput, View} from "react-native";
 import TextBox from "../Components/TextBox";
 import ButtonBox from "../Components/ButtonBox";
 import styles, {colors} from "../../Resources/Styles";
+import {createStore} from "redux";
+
+const ADD = 'ADD',
+    SUBTRACT = 'SUBTRACT',
+    MULTIPLY = 'MULTIPLY',
+    DIVIDE = 'DIVIDE',
+    SUM = 'SUM';
+
+const initialState = {
+    a: 0,
+    b: 0,
+    result: 0,
+    action: ''
+};
+
+const getSum = (action, a, b) => {
+    switch (action) {
+        case ADD:
+            return a + b;
+        case SUBTRACT:
+            return a - b;
+        case MULTIPLY:
+            return a * b;
+        case DIVIDE:
+            return a / b;
+        default:
+            return a;
+    }
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD:
+            return Object.assign({}, state, {
+                a: action.a,
+                action: ADD
+            });
+        case SUBTRACT:
+            return Object.assign({}, state, {
+                a: action.a,
+                action: SUBTRACT
+            });
+        case MULTIPLY:
+            return Object.assign({}, state, {
+                a: action.a,
+                action: MULTIPLY
+            });
+        case DIVIDE:
+            return Object.assign({}, state, {
+                a: action.a,
+                action: DIVIDE
+            });
+        case SUM:
+            let result = getSum(state.action, state.a, action.b);
+            return Object.assign({}, state, {
+                b: action.b,
+                result: result
+            });
+        default:
+            return state;
+    }
+};
+
+const store = createStore(reducer);
 
 class CustomButton extends Component {
     constructor(props) {
@@ -29,26 +93,6 @@ class CustomButton extends Component {
 
 class Numbers extends Component {
 
-    onDivide = () => {
-        ToastAndroid.show('Divide button clicked', ToastAndroid.SHORT);
-    };
-
-    onMultiply = () => {
-        ToastAndroid.show('Multiply button clicked', ToastAndroid.SHORT);
-    };
-
-    onSubtract = () => {
-        ToastAndroid.show('Subtract button clicked', ToastAndroid.SHORT);
-    };
-
-    onEquals = () => {
-        ToastAndroid.show('Equal button clicked', ToastAndroid.SHORT);
-    };
-
-    onAdd = () => {
-        ToastAndroid.show('Add button clicked', ToastAndroid.SHORT);
-    };
-
     render() {
         const containerStyle = {
             flex: 1,
@@ -61,11 +105,11 @@ class Numbers extends Component {
         return (
             <View style={containerStyle}>
                 <View style={rowStyle}>
-                    <CustomButton title="+" onPress={this.onAdd}/>
-                    <CustomButton title="-" onPress={this.onSubtract}/>
-                    <CustomButton title="*" onPress={this.onMultiply}/>
-                    <CustomButton title="/" onPress={this.onDivide}/>
-                    <CustomButton title="=" onPress={this.onEquals}/>
+                    <CustomButton title="+" onPress={this.props.onAdd}/>
+                    <CustomButton title="-" onPress={this.props.onSubtract}/>
+                    <CustomButton title="*" onPress={this.props.onMultiply}/>
+                    <CustomButton title="/" onPress={this.props.onDivide}/>
+                    <CustomButton title="=" onPress={this.props.onEquals}/>
                 </View>
             </View>
         );
@@ -77,8 +121,9 @@ export default class CalculatorScene extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            visibleText: '',
-            text: ''
+            input: '',
+            output: '',
+            ...store.getState()
         };
     }
 
@@ -86,29 +131,54 @@ export default class CalculatorScene extends Component {
         this.props.navigator.pop();
     }
 
-    submitText(text) {
-        this.setState({text})
-    }
+    onDivide = () => {
+        let a = parseInt(this.state.input);
+        store.dispatch({type: DIVIDE, a: a});
+        this.setState({output: ''});
+    };
 
-    displayText() {
-        this.setState((prevState) => ({
-            visibleText: prevState.text
-        }));
-    }
+    onMultiply = () => {
+        let a = parseInt(this.state.input);
+        store.dispatch({type: MULTIPLY, a: a});
+        this.setState({output: ''});
+    };
+
+    onSubtract = () => {
+        let a = parseInt(this.state.input);
+        store.dispatch({type: SUBTRACT, a: a});
+        this.setState({output: ''});
+    };
+
+    onAdd = () => {
+        let a = parseInt(this.state.input);
+        store.dispatch({type: ADD, a: a});
+        this.setState({output: ''});
+    };
+
+    onEquals = () => {
+        let b = parseInt(this.state.input);
+        store.dispatch({type: SUM, b: b});
+        this.setState({output: '', ...store.getState()});
+    };
 
     render() {
         return (
             <View style={styles.container}>
+                <TextBox
+                    title={this.state.result}/>
                 <TextInput
+                    value={this.state.output}
                     keyboardType='numeric'
                     style={{backgroundColor: 'white'}}
-                    onChangeText={(text) => this.submitText(text)}/>
-                <TextBox
-                    title={this.state.visibleText}/>
-                <Numbers/>
-                <ButtonBox
-                    title='Receive Text'
-                    onClick={() => this.displayText()}/>
+                    onChangeText={(text) => {
+                        this.setState({input: text, output: text})
+                    }}/>
+                <Numbers
+                    onAdd={this.onAdd}
+                    onSubtract={this.onSubtract}
+                    onMultiply={this.onMultiply}
+                    onDivide={this.onDivide}
+                    onEquals={this.onEquals}/>
                 <ButtonBox
                     title='Back'
                     onClick={() => this.toMain()}/>
